14c22754bca1b2d5224d44eaec393c6fd4de6da1
이건호 <hoyahoya0517@naver.com>
2023-11-13 19:37:01 +0900

    verLogin1.1

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,17 +3,30 @@ import styles from "./Login.module.css";
 import { useDispatch } from "react-redux";
 import { navOff } from "../../redux/redux";
 import { useState, useEffect } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { login } from "../../api/calendar";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { login, me } from "../../api/calendar";
 import { useNavigate } from "react-router-dom";
 
 export default function Login(): JSX.Element {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const user = queryClient.getQueryData(["user"]);
-  useEffect(() => {
-    if (user) navigate(-1);
-  }, []);
+  const { isLoading, data } = useQuery(
+    ["user"],
+    async () => {
+      const data = await me();
+      return data;
+    },
+    {
+      onSuccess() {
+        console.log("autoLogin success");
+      },
+      onError() {
+        console.log("autoLogin error");
+        queryClient.setQueryData(["user"], null);
+      },
+      retry: 1,
+    }
+  );
   const loginMutate = useMutation({
     mutationFn: ({ code }: { code: string | undefined }) => login(code),
     onSuccess() {
@@ -43,6 +56,7 @@ export default function Login(): JSX.Element {
     setCode(undefined);
     loginMutate.mutate({ code });
   };
+  if (data) navigate(-1);
   return (
     <div className={styles.login}>
       <video
@@ -50,6 +64,7 @@ export default function Login(): JSX.Element {
         autoPlay
         muted
         loop
+        playsInline
         className={styles.loginVideo}
       >
         <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1699448014/hy/videoplayback_cut_1_hotwwi.mp4"></source>
